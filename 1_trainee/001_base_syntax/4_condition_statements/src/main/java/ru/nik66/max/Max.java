3140411fb0fced176670e42883444078c22fc01a
Nikolay Kotkin <nikbstar@gmail.com>
2017-12-24 16:46:18 +0500

    3.2. Максимум из двух чисел [#189]

---
+++ 1_trainee/001_base_syntax/4_condition_statements/src/main/java/ru/nik66/max/Max.java
@@ -0,0 +1,9 @@
+package ru.nik66.max;
+
+public class Max {
+
+    public int max(int first, int second) {
+        return (first >= second) ? first : second;
+    }
+
+}
---
+++ 1_trainee/001_base_syntax/4_condition_statements/src/test/java/ru/nik66/max/MaxTest.java
@@ -0,0 +1,40 @@
+package ru.nik66.max;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MaxTest {
+
+    @Test
+    public void whenFirstIsGreaterThanSecondThenReturnFirst() throws Exception {
+        Max max = new Max();
+
+        int actual = max.max(1, 0);
+        int expected = 1;
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenSecondIsGreaterThanSecondThenReturnSecond() throws Exception {
+        Max max = new Max();
+
+        int actual = max.max(0, 1);
+        int expected = 1;
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenBothAreEqualThenReturnFirst() throws Exception {
+        Max max = new Max();
+
+        int actual = max.max(0, 0);
+        int expected = 0;
+
+        assertThat(actual, is(expected));
+    }
+
+}

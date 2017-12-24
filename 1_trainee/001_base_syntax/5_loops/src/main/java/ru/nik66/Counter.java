2239c8b4fa731bff6f7d6bdef47b4aaeba8aa647
Nikolay Kotkin <nikbstar@gmail.com>
2017-12-24 20:45:31 +0500

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ 1_trainee/001_base_syntax/5_loops/src/main/java/ru/nik66/Counter.java
@@ -0,0 +1,15 @@
+package ru.nik66;
+
+public class Counter {
+
+    public int add(int start, int finish) {
+        int result = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ 1_trainee/001_base_syntax/5_loops/src/test/java/ru/nik66/CounterTest.java
@@ -0,0 +1,21 @@
+package ru.nik66;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter counter = new Counter();
+
+        int actual = counter.add(1, 10);
+        int expected = 30;
+
+        assertThat(actual, is(expected));
+    }
+
+
+}

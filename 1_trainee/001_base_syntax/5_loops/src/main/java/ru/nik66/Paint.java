ac2d949b32d4ceba933c760b19a90c90471b7750
Nikolay Kotkin <nikbstar@gmail.com>
2017-12-25 21:32:58 +0500

    4.5. Рефакторинг кода. [#33459]

--- 1_trainee/001_base_syntax/5_loops/src/main/java/ru/nik66/Paint.java
+++ 1_trainee/001_base_syntax/5_loops/src/main/java/ru/nik66/Paint.java
@@ -1,25 +1,74 @@
 package ru.nik66;
 
+import java.util.function.BiPredicate;
+
 public class Paint {
 
     public String pyramid(int h) {
-        StringBuilder sb = new StringBuilder();
-        String eol = System.lineSeparator();
-        int w = (h * 2) - 1;
+//        StringBuilder sb = new StringBuilder();
+//        String eol = System.lineSeparator();
+//        int w = (h * 2) - 1;
+//
+//        for (int i = 0; i < h; i++) {
+//            for (int j = 1; j <= w; j++) {
+//                if ((j >= h - i) && (j <= h + i)) {
+//                    sb.append("^");
+//                } else {
+//                    sb.append(" ");
+//                }
+//            }
+//            sb.append(eol);
+//        }
+//        return sb.toString();
+        return this.loopBy(h, (h * 2) - 1, (i, j) -> ((j + 1 >= h - i) && (j + 1 <= h + i)));
+    }
 
-        for (int i = 0; i < h; i++) {
-            for (int j = 1; j <= w; j++) {
-                if ((j >= h - i) && (j <= h + i)) {
+    public String rightTrl(int height) {
+//        StringBuilder sb = new StringBuilder();
+//        int width = height;
+//        for (int row = 0; row != height; row++) {
+//            for (int column = 0; column != width; column++) {
+//                if (row >= column) {
+//                    sb.append("^");
+//                } else {
+//                    sb.append(" ");
+//                }
+//            }
+//            sb.append(System.lineSeparator());
+//        }
+//        return sb.toString();
+        return this.loopBy(height, height, (row, col) -> row >= col);
+    }
+
+    public String leftTrl(int height) {
+//        StringBuilder sb = new StringBuilder();
+//        int width = height;
+//        for (int i = 0; i < height; i++) {
+//            for (int j = 0; j < width; j++) {
+//                if (i >= width - j - 1) {
+//                    sb.append("^");
+//                } else {
+//                    sb.append(" ");
+//                }
+//            }
+//            sb.append(System.lineSeparator());
+//        }
+//        return sb.toString();
+        return this.loopBy(height, height, (row, col) -> row >= height - col - 1);
+    }
+
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predicate) {
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row != height; row++) {
+            for (int col = 0; col != width; col++) {
+                if (predicate.test(row, col)) {
                     sb.append("^");
                 } else {
                     sb.append(" ");
                 }
             }
-            sb.append(eol);
+            sb.append(System.lineSeparator());
         }
-        
-        
-        // TODO
         return sb.toString();
     }
 
--- 1_trainee/001_base_syntax/5_loops/src/test/java/ru/nik66/PaintTest.java
+++ 1_trainee/001_base_syntax/5_loops/src/test/java/ru/nik66/PaintTest.java
@@ -2,10 +2,46 @@ package ru.nik66;
 
 import org.junit.Test;
 
+import java.util.StringJoiner;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class PaintTest {
+
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String eol = System.lineSeparator();
+
+        String actual = paint.rightTrl(4);
+        System.out.println(actual);
+        String expected = new StringJoiner(eol, "", eol)
+                .add("^   ")
+                .add("^^  ")
+                .add("^^^ ")
+                .add("^^^^")
+                .toString();
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String eol = System.lineSeparator();
+
+        String actual = paint.leftTrl(4);
+        System.out.println(actual);
+        String expected = new StringJoiner(eol, "", eol)
+                .add("   ^")
+                .add("  ^^")
+                .add(" ^^^")
+                .add("^^^^")
+                .toString();
+
+        assertThat(actual, is(expected));
+    }
 
     @Test
     public void whenPyramid2LevelsThen() {
@@ -13,6 +49,7 @@ public class PaintTest {
         String eol = System.lineSeparator();
 
         String actual = paint.pyramid(2);
+        System.out.println(actual);
         String expected = String.format(" ^ %s^^^%s", eol, eol);
 
         assertThat(actual, is(expected));
@@ -21,10 +58,14 @@ public class PaintTest {
     @Test
     public void whenPyramid3LevelsThen() {
         Paint paint = new Paint();
-        String eol = System.lineSeparator();
 
         String actual = paint.pyramid(3);
-        String expected = String.format("  ^  %s ^^^ %s^^^^^%s", eol, eol, eol);
+        System.out.println(actual);
+        String expected = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("  ^  ")
+                .add(" ^^^ ")
+                .add("^^^^^")
+                .toString();
 
         assertThat(actual, is(expected));
     }

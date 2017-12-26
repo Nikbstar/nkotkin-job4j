855867a20eb75fa8efd140d0e423f97a9f46b4ae
Nikbstar <Nikbstar@gmail.com>
2017-12-26 12:12:09 +0500

    5.0. Заполнить массив степенями чисел. [#33488]

---
+++ 1_trainee/001_base_syntax/6_arrays/src/test/java/ru/nik66/SquareTest.java
@@ -0,0 +1,20 @@
+package ru.nik66;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SquareTest {
+
+    @Test
+    public void whenThen() throws Exception {
+        Square square = new Square();
+
+        int[] actual = square.calculate(2);
+        int[] expected = {1, 4};
+
+        assertThat(actual, is(expected));
+    }
+
+}

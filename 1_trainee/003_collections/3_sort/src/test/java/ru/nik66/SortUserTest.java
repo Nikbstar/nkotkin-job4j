333491483d51d71ef1942608404b133478dd4b1c
Nikolay Kotkin <nikbstar@gmail.com>
2018-03-29 23:31:08 +0500

    2. Сортировка User с использованием Comparator [#10036]

--- 1_trainee/003_collections/3_sort/src/test/java/ru/nik66/SortUserTest.java
+++ 1_trainee/003_collections/3_sort/src/test/java/ru/nik66/SortUserTest.java
@@ -19,7 +19,7 @@ public class SortUserTest {
     @Before
     public void init() {
         this.user1 = new User("Kolya", 22);
-        this.user2 = new User("Katya", 11);
+        this.user2 = new User("Kat", 11);
     }
 
     @Test
@@ -36,6 +36,26 @@ public class SortUserTest {
     @Test
     public void whenSortUsersByAgeThenMinus() {
         assertThat(user1.compareTo(user2), is(11));
+    }
+
+    @Test
+    public void whenSortByNameLength() {
+        List<User> users = Arrays.asList(this.user1, this.user2);
+        SortUser sort = new SortUser();
+        List<User> actual = sort.sortNameLength(users);
+        List<User> expected = Arrays.asList(this.user2, this.user1);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenSortByAllFields() {
+        User user3 = new User("Kolya", 15);
+        User user4 = new User("Kat", 55);
+        List<User> users = Arrays.asList(this.user1, this.user2, user3, user4);
+        SortUser sort = new SortUser();
+        List<User> actual = sort.sortByAllFields(users);
+        List<User> expected = Arrays.asList(this.user2, user4, user3, this.user1);
+        assertThat(actual, is(expected));
     }
 
 }

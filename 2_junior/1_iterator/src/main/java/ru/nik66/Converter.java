e66ac2ad88c615f1317e528746b214c74342e487
Nikolay Kotkin <nikbstar@gmail.com>
2018-04-10 21:29:21 +0500

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

---
+++ 2_junior/1_iterator/src/main/java/ru/nik66/Converter.java
@@ -0,0 +1,46 @@
+package ru.nik66;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> its) {
+        return new Iterator<Integer>() {
+
+            private Iterator<Integer> next = null;
+            private Iterator<Integer> tmp = null;
+
+            {
+                if (its.hasNext()) {
+                    this.next = its.next();
+                }
+                if (its.hasNext()) {
+                    this.tmp = its.next();
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                if (this.next != null && (this.next.hasNext() || (this.tmp != null && this.tmp.hasNext()))) {
+                    result = true;
+                }
+                return result;
+            }
+
+            @Override
+            public Integer next() {
+                int result;
+                if (this.tmp == null && !this.next.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (!this.next.hasNext()) {
+                    this.next = this.tmp;
+                    this.tmp = its.hasNext() ? its.next() : null;
+                }
+                result = this.next.next();
+                return result;
+            }
+        };
+    }
+}

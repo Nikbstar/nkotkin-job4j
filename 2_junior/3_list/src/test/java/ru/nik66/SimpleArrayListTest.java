eda23a5041de11ab3ee41dc42cf60d1f01cd8cd9
Nikolay Kotkin <nikbstar@gmail.com>
2018-04-21 14:45:56 +0500

    5.3.1. Создать динамический список на базе массива. [#158]

---
+++ 2_junior/3_list/src/test/java/ru/nik66/SimpleArrayListTest.java
@@ -0,0 +1,52 @@
+package ru.nik66;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SimpleArrayListTest {
+
+    @Test
+    public void whenCreateSimpleArrayListThenLengthIsTen() {
+        SimpleArrayList<String> myList = new SimpleArrayList<>();
+        int actual = myList.getLength();
+        int expected = 10;
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void whenCreateSimpleArrayListWithZeroLengthAndAddElements() {
+        SimpleArrayList<String> myList = new SimpleArrayList<>(0);
+        assertThat(myList.getLength(), is(0));
+        myList.add("1");
+        assertThat(myList.getLength(), is(1));
+        myList.add("1");
+        assertThat(myList.getLength(), is(2));
+        myList.add("1");
+        assertThat(myList.getLength(), is(4));
+        myList.add("1");
+        assertThat(myList.getLength(), is(4));
+    }
+
+    @Test
+    public void whenGetElement() {
+        SimpleArrayList<String> myList = new SimpleArrayList<>();
+        myList.add("1");
+        String actual = myList.get(0);
+        String expected = "1";
+        assertThat(actual, is(expected));
+    }
+
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenChangeListWhileIteratorThenThrowConcurrentModificationException() {
+        SimpleArrayList<String> myList = new SimpleArrayList<>();
+        myList.add("1");
+        for (String s : myList) {
+            myList.add("2");
+        }
+    }
+
+}

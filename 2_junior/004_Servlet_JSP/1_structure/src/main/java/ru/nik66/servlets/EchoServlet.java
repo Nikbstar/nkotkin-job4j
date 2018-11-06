c3dd4420090fed052818528577d43fbf95aaf260
Nikolay Kotkin <nikbstar@gmail.com>
2018-11-06 22:10:22 +0500

    1. Создать web проект [#2514] add

--- 2_junior/004_Servlet_JSP/1_structure/src/main/java/ru/nik66/servlets/EchoServlet.java
+++ 2_junior/004_Servlet_JSP/1_structure/src/main/java/ru/nik66/servlets/EchoServlet.java
@@ -6,14 +6,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class EchoServlet extends HttpServlet {
+
+    private List<String> names = new CopyOnWriteArrayList<>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("hello world");
+        writer.append("hello world ").append(this.names.toString());
         writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.names.add(req.getParameter("name"));
+        doGet(req, resp);
     }
 }

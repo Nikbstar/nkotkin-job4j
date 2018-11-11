f7052025a8e5b2c84419b891f3c65248da3b2de5
Nikolay Kotkin <nikbstar@gmail.com>
2018-11-11 22:22:41 +0500

    1. Создать web проект [#2514] (home) add

--- 2_junior/004_Servlet_JSP/1_structure/src/main/java/ru/nik66/servlets/EchoServlet.java
+++ 2_junior/004_Servlet_JSP/1_structure/src/main/java/ru/nik66/servlets/EchoServlet.java
@@ -16,9 +16,26 @@ public class EchoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("hello world ").append(this.names.toString());
+        StringBuilder sb = new StringBuilder("<table>");
+        for (String name : this.names) {
+            sb.append("<tr><td>").append(name).append("</td></tr>");
+        }
+        sb.append("</table>");
+        writer.append("<!DOCTYPE html>");
+        writer.append("<html lang='en'>");
+        writer.append("<head>");
+        writer.append("<meta charset='UTF-8' />");
+        writer.append("<title>Echo Servlet</title>");
+        writer.append("</head>");
+        writer.append("<body>");
+        writer.append("<form action='").append(req.getContextPath()).append("/echo' method='POST'>");
+        writer.append("Name: <input type='text' name='name'>").append("<br />");
+        writer.append("<input type='submit' />" );
+        writer.append("</form>").append("<hr />");
+        writer.append(sb.toString());
+        writer.append("</body>");
+        writer.append("<html lang='en'>");
         writer.flush();
     }
 
